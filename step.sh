ade6bee64218c2eb9c72d775902024e954f4e642
Bitrise Buildbot (Firebase remote-config backup) <cpeyer@mac.com>
2023-01-12 08:53:16 +0100

    do not execute when there are no changes

--- step.sh
+++ step.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 #=======================================
 # Functions
@@ -79,6 +78,29 @@ function validate_required_input_with_options {
 }
 
 #=======================================
+# Helper Functions
+#=======================================
+
+function execute {
+  cmd=$1
+  err_msg=$2
+
+  echo_details "$cmd"
+  echo
+
+  eval "${cmd}"
+
+  if [ $? -ne 0 ] ; then
+      echo_fail "$err_msg"
+  fi
+}
+
+function delete_branch {
+    branch_name=$1
+    echo "  ${msg}"
+}
+
+#=======================================
 # Main
 #=======================================
 
@@ -121,58 +143,84 @@ if [ -n "${service_credentials_file}" ] ; then
     export GOOGLE_APPLICATION_CREDENTIALS="${service_credentials_file}"
 fi
 
-# Deploy
+# Export
+#
 echo_info "Exporting Firebase remote-config"
+echo
 
 remote_config_file="${project_id}_remote_config.json"
 
-submit_cmd="firebase remoteconfig:get"
-submit_cmd="$submit_cmd --project \"${project_id}\" -o \"${remote_config_file}\""
+firebase_cmd="firebase remoteconfig:get"
+firebase_cmd="$firebase_cmd --project \"${project_id}\" -o \"${remote_config_file}\""
 
 # Optional params
 if [ "${is_debug}" = true ] ; then
-    submit_cmd="$submit_cmd --debug"
+    firebase_cmd="$firebase_cmd --debug"
 fi
 
-echo_details "$submit_cmd"
-echo
-
-eval "${submit_cmd}"
-
-if [ $? -ne 0 ] ; then
-    echo_fail "Fail"
-fi
+execute "$firebase_cmd" "Cannot export remote-config from Firebase."
 
 # Backup to repository
 echo_info "Backup Firebase remote-config to repository"
 echo
 
-echo_info "Create \"firebase_remote_config_backup\" branch"
+### Check for changes
+#
+check_cmd="git diff-index --quiet HEAD"
 
-branch_cmd="git checkout -b firebase_remote_config_backup HEAD"
-
-echo_details "$branch_cmd"
+echo_details "$check_cmd"
 echo
 
-eval "${branch_cmd}"
+eval "$check_cmd"
 
-if [ $? -ne 0 ] ; then
-    echo_fail "Fail"
+if [ $? -eq 0 ]; then
+    echo_done "No changes. Nothing to do."
 fi
 
+### Create Branch
+#
+backup_branch_name="firebase_remote_config_backup"
+
+## cleanup (not used at the moment)
+#old_branch=$(git branch --show-current)
+#cleanup_cmd="git checkout \"$old_branch\""
+#cleanup_cmd="$cleanup_cmd; git branch --delete \"$backup_branch_name\""
+#cleanup_cmd="$cleanup_cmd; git push origin -d \"$backup_branch_name\""
+
+echo_info "Create \"$backup_branch_name\" branch"
+
+branch_cmd="git checkout -b \"$backup_branch_name\" HEAD"
+
+execute "$branch_cmd" "Cannot create branch."
+
+### Commit & Push
+#
 echo_info "Add and commit \"${remote_config_file}\""
 
 commit_cmd="git add \"${remote_config_file}\""
-commit_cmd="$commit_cmd; git diff-index --quiet HEAD || git commit -m \"Firebase remote-config backup\""
+commit_cmd="$commit_cmd; git commit -m \"Firebase remote-config backup\""
 commit_cmd="$commit_cmd; git push -u origin HEAD"
 
-echo_details "$commit_cmd"
-echo
+execute "$commit_cmd" "Cannot commit."
 
-eval "${commit_cmd}"
+### Install GH
+#
+echo_info "Install GH"
 
-if [ $? -eq 0 ] ; then
-    echo_done "Success"
-else
-    echo_fail "Fail"
-fi
+install_gh_cmd="curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y"
+
+execute "$install_gh_cmd" "Cannot install GH."
+
+### Create PR
+#
+create_pr_cmd="echo \"$SRF_APPS_GITHUB_TOKEN\" > .githubtoken"
+create_pr_cmd="$create_pr_cmd; gh auth login --with-token < .githubtoken"
+create_pr_cmd="gh pr create --fill"
+
+execute "$create_pr_cmd" "Cannot create PR."
+
+echo_done "Success"
